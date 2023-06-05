222e9a984675430407e7ace25711bce2e99fb9f9
gchoi <gchoi@utinfra.com>
2023-06-05 17:28:43 +0900

    edited authhcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ const login = asyncHandler(async (req, res) => {
 
     const refreshToken = jwt.sign(
         { "username": foundUser.username },
-        process.env.REFRESH_TOKEN_SECRET,
+        process.env.REFRESH_ACCESS_TOKEN_SECRET,
         { expiresIn: '7d' }
     )
 
